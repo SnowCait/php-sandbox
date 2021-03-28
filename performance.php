e9d2f12d4c831d9ae8522d96cbc3797113d6ba41
雪猫 <SnowCait@users.noreply.github.com>
2021-03-28 12:49:51 +0900

    Update performance.php

--- performance.php
+++ performance.php
@@ -6,13 +6,19 @@ $size = 1000000;
 $repeat = max(array(1, intval(5000000 / $size)));
 $targetArray = range(1, $size);
 
+$s microtime(true);
+for ($i = 0; $i < $repeat, $i++) {
+    function ($targetArray);
+}
+$framework_time = microtime(true) - $s;
+
 $functions = include('performance/array.php');
 $max_length = max(array_map(function ($str) { return strlen($str); }, array_keys($functions)));
 foreach ($functions as $title => $function) {
     $s = microtime(true);
-    for ($i=0; $i < $repeat; $i++) {
+    for ($i = 0; $i < $repeat; $i++) {
         $function($targetArray);
     }
     $t = microtime(true) - $s;
-    printf("%s: %0.4fms\n", str_pad($title, $max_length), $t * 1000 / $repeat);
+    printf("%s: %0.4fms\n", str_pad($title, $max_length), ($t - $framework_time) * 1000 / $repeat);
 }
