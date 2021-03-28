31892e9429ddff42586688f493a9fcd63c5ef277
雪猫 <SnowCait@users.noreply.github.com>
2021-03-28 12:38:05 +0900

    Update performance.php

--- performance.php
+++ performance.php
@@ -1,6 +1,4 @@
 <?php
-declare(strict_types=1);
-
 // http://tanakahisateru.hatenablog.jp/entry/2016/05/25/232422
 
 $size = 1000000;
@@ -9,7 +7,7 @@ $repeat = max([1, intval(5000000 / $size)]);
 $targetArray = range(1, $size);
 
 $functions = include('performance/array.php');
-$max_length = max(array_map(function (string $str): int { return strlen($str); }, array_keys($functions)));
+$max_length = max(array_map(function ($str) { return strlen($str); }, array_keys($functions)));
 foreach ($functions as $title => $function) {
     $s = microtime(true);
     for ($i=0; $i < $repeat; $i++) {
