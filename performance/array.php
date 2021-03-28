875cf691b6572f621b1585b2720dcfabc0ba3901
雪猫 <SnowCait@users.noreply.github.com>
2021-03-28 12:13:03 +0900

    Create array.php

---
+++ performance/array.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_type=1);
+
+// http://tanakahisateru.hatenablog.jp/entry/2016/05/25/232422
+
+function plus1(int $n): int {
+    return $n + 1;
+}
+
+return [
+    function (array $targetArray): array {
+        return array_map(function(int $n): int { return $n + 1; }, $targetArray);
+    },
+    function (array $targetArray): array {
+        return array_map('plus1', $targetArray);
+    },
+    function (array $targetArray): array {
+        $buf = [];
+        foreach ($targetArray as $k => $v) {
+            $buf[$k] = $v + 1;
+        }
+        return $buf;
+    },
+    function (array $targetArray): array {
+        $buf = [];
+        foreach ($targetArray as $v) {
+            $buf[] = $v + 1;
+        }
+        return $buf;
+    },
+];
