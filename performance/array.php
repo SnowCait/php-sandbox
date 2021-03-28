1c07579262d1617adefae0af86e8442c0bdf82bb
雪猫 <SnowCait@users.noreply.github.com>
2021-03-28 12:37:40 +0900

    Update array.php

--- performance/array.php
+++ performance/array.php
@@ -1,27 +1,25 @@
 <?php
-declare(strict_types=1);
-
 // http://tanakahisateru.hatenablog.jp/entry/2016/05/25/232422
 
-function plus1(int $n): int {
+function plus1($n) {
     return $n + 1;
 }
 
 return [
-    'array_map + Closure' => function (array $targetArray): array {
-        return array_map(function(int $n): int { return $n + 1; }, $targetArray);
+    'array_map + Closure' => function ($targetArray) {
+        return array_map(function($n) { return $n + 1; }, $targetArray);
     },
-    'array_map + function' => function (array $targetArray): array {
+    'array_map + function' => function ($targetArray) {
         return array_map('plus1', $targetArray);
     },
-    'foreach with key' => function (array $targetArray): array {
+    'foreach with key' => function ($targetArray) {
         $buf = [];
         foreach ($targetArray as $k => $v) {
             $buf[$k] = $v + 1;
         }
         return $buf;
     },
-    'foreach without key' => function (array $targetArray): array {
+    'foreach without key' => function ($targetArray) {
         $buf = [];
         foreach ($targetArray as $v) {
             $buf[] = $v + 1;
