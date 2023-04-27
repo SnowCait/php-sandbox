4fea5bf8f5ee63e1ea23e9c18771468611b9fbf7
SnowCait <SnowCait@users.noreply.github.com>
2023-04-28 04:22:24 +0900

    Send event

--- nostr/nostr.php
+++ nostr/nostr.php
@@ -2,8 +2,26 @@
 require_once __DIR__ . '/vendor/autoload.php';
 
 use swentel\nostr\Key\Key;
+use swentel\nostr\Event\Event;
+use swentel\nostr\Sign\Sign;
+use swentel\nostr\Message\EventMessage;
+use swentel\nostr\Relay\Relay;
 
 $public_key = 'npub10elfcs4fr0l0r8af98jlmgdh9c8tcxjvz9qkw038js35mp4dma8qzvjptg';
 $key = new Key();
 $hex = $key->convertToHex($public_key);
 var_dump($hex);
+
+$nsec = '';
+$private_key = $key->convertToHex($nsec);
+
+$event = new Event();
+$event->setContent('Hello world');
+$event->setKind(1);
+$signer = new Sign();
+$signer->signEvent($event, $private_key);
+$eventMessage = new EventMessage($event);
+
+$websocket = 'wss://example.com';
+$relay = new Relay($websocket, $eventMessage);
+$result = $relay->send();
